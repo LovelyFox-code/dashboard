257cfb5117a48791ff8e2c30e0fb29a13b1dd4c3
Alina <alinadakhno60@gmail.com>
2022-06-01 18:17:17 +0200

    grid

---
+++ src/components/Grid/Grid.jsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+width: 90%;
+max-width: 1110px;
+margin: auto;
+`;
+
+export const Row = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 30px;
+  margin: 25px 0;
+`;
+
+export const Div = styled.div`
+width: auto;
+`;
