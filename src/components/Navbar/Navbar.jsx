b7368b1a5518c17342cd5c6206a0797996d32106
Alina <alinadakhno60@gmail.com>
2022-06-01 18:44:42 +0200

    navbar with styled component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,20 +1,32 @@
 import React from 'react';
 import { ModeBtn,Input, Label } from './ModeBtn';
-import style from './Navbar.module.css';
+import styled from "styled-components";
+import { Row } from '../Grid/Grid';
+import {H1, P} from '../Typography/Typography'
+ 
+    const Nav = styled.section`
+        display: flex;
+        align-items: center;
+        justify-content: space-between;
+        padding: 3% 0;
+    `;
+    const Logo =styled.div`
+        width: auto;
+    `
 function Navbar() {
     return (
-        <div className={style.nav}>
-            <div className={style.logo}>
-            <h1 >Social Media Dashboard</h1>
-            <p>Total Followers: 23,004</p>
-            </div>
-            <div className={style.row}>
+        <Nav>
+            <Logo>
+            <H1>Social Media Dashboard</H1>
+            <P>Total Followers: 23,004</P>
+            </Logo>
+            <Row>
             <ModeBtn>
                 <Input/>
                 <Label></Label>
             </ModeBtn>
-            </div>
-        </div>
+            </Row>
+        </Nav>
 
        
     )
