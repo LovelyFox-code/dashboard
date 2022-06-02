0e28c3fe56bfacedc4e78a82c0489524ae56c63b
Alina <alinadakhno60@gmail.com>
2022-06-02 11:48:43 +0200

    Big box with styled component, delete module.css

--- src/components/BigBox/BigBox.jsx
+++ src/components/BigBox/BigBox.jsx
@@ -1,55 +1,76 @@
 import React from "react";
-import style from "./BigBox.module.css";
+import styled from "styled-components";
+import { Row, DivCenter } from "../Grid/Grid";
+import { H3, P, Span } from "../Typography/Typography";
+
+const Box = styled.div`
+  width: 200px;
+  background: #252b42;
+  border-radius: 5px;
+  padding: 35px 46px 24px 46px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+const Number = styled.h1`
+  font-family: "Inter";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 56px;
+  line-height: 48px;
+  text-align: center;
+  letter-spacing: -2px;
+  color: #ffffff;
+`;
 
 
 function BigBox(props) {
-
-    return <div className={style.boxContainer}>
-                <div className={style.box}>
-            <div className={style.social}>
-                <div className={style.logo}>
-                    <svg
-                        width="20"
-                        height="20"
-                        viewBox="0 0 20 20"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            id="Path"
-                            d={props.icon}
-                            fill={props.fill}
-                        />
-                    </svg>
-                </div>
-                <p>@{props.user}</p>
-            </div>
-            <div className={style.followers}>
-                <h1>{props.followers}</h1>
-                <h3>Followers</h3>
-            </div>
-            <div className={style.scale}>
-                <div className={style.arrow}>
-                    <svg
-                        width="8"
-                        height="4"
-                        viewBox="0 0 8 4"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                    >
-                        <path
-                            id="Path 3"
-                            fill-rule="evenodd"
-                            clip-rule="evenodd"
-                            d="M0 4L4 0L8 4H0Z"
-                            fill="#1EB589"
-                        />
-                    </svg>
-                </div>
-                <div> <span></span> Today</div>
-            </div>
+    
+  return (
+    <Box>
+      <Row smallGap>
+        <div>
+          <svg
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path id="Path" d={props.icon} fill={props.fill} />
+          </svg>
         </div>
-    </div>;
+        <P>@{props.user}</P>
+      </Row>
+      <DivCenter>
+        <Number>{props.followers}</Number>
+        <H3>Followers</H3>
+      </DivCenter>
+      <Row smallGap>
+        <DivCenter>
+          <svg
+            width="8"
+            height="4"
+            viewBox="0 0 8 4"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              id="Path 3"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M0 4L4 0L8 4H0Z"
+              fill="#1EB589"
+            />
+          </svg>
+        </DivCenter>
+        <P>
+          
+          <Span>{props.changeToday}</Span> Today
+        </P>
+      </Row>
+    </Box>
+  );
 }
 
 export default BigBox;
